d7d5ecde4559a5680ce249bd491f3f085adf41c8
Matias C <mec.058@gmail.com>
2023-09-13 21:03:35 -0300

    routine model

--- LC_Web/routine/models.py
+++ LC_Web/routine/models.py
@@ -1,2 +1,10 @@
 from django.db import models
 
+class Routines(models.Model):
+    title = models.CharField(max_length=200)
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
+    objetivo = models.TextField(max_length=500)
+    observaciones = models.TextField(max_length=500)
+    is_active = models.BooleanField(default=False)
+    slug_routine = models.SlugField(max_length=200, unique='True')

902dd1de18b784ac680d30b301e5125c5d139d47
Matias C <mec.058@gmail.com>
2023-09-16 20:36:44 -0300

    models rutina

--- LC_Web/routine/models.py
+++ LC_Web/routine/models.py
@@ -1,4 +1,13 @@
+import os
 from django.db import models
+from django.forms import ValidationError
+
+
+def validate_file_extension_photo(value):
+    accepted_extensions = [ '.jpg', '.jpeg', '.png' ]
+    file_extension = os.path.splitext(value.name)[1].lower()
+    if file_extension not in accepted_extensions:
+        raise ValidationError(f"Formato no Soportado. Solo {', '.join(accepted_extensions)} permitido.")
 
 class Routines(models.Model):
     title = models.CharField(max_length=200)
@@ -8,3 +17,5 @@ class Routines(models.Model):
     observaciones = models.TextField(max_length=500)
     is_active = models.BooleanField(default=False)
     slug_routine = models.SlugField(max_length=200, unique='True')
+    routine_cover = models.ImageField(upload_to='routine/', validators=[validate_file_extension_photo])
+    video_link = models.URLField(max_length=100, blank=True)

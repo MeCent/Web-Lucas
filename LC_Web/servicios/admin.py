d577417722f53557d98230dc65be08d1012135ee
Matias C <mec.058@gmail.com>
2024-02-13 20:35:12 -0300

    de vuelta

--- LC_Web/servicios/admin.py
+++ LC_Web/servicios/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Service
+from .models import Service, Carousel
 
 class ServiceAdmin(admin.ModelAdmin):
-    list_display = ('title', 'subtitle', 'description', 'benefits', 'example_photos')
+    list_display = ('title', 'subtitle', 'description', 'benefits', 'image')
+
+class CarouselAdmin(admin.ModelAdmin):
+    list_display = ('image', 'title', 'subtitle')
 
 admin.site.register(Service, ServiceAdmin)
+
+admin.site.register(Carousel, CarouselAdmin)
--- LC_Web/servicios/views.py
+++ LC_Web/servicios/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from .models import Service
+from .models import Service, Carousel
 
 def servicios(request):
     service = Service.objects.all()
+    carousel = Carousel.objects.all()
     data = {
         'service': service,
+        'carousel': carousel,
     }
     return render(request, 'pages/servicios.html', data)
